912abec5df1b135391a8d1bf04d469cb337643e5
Fabricio Castro <fabriciofeitosacastro@gmail.com>
2024-04-21 22:49:36 -0300

    Create card avatar

--- src/pages/Home/image_slider.ts
+++ src/pages/Home/image_slider.ts
@@ -1,23 +1,28 @@
-import Acupuncture from '../../components/assets/acupuncture.jpg'
 import massage from '../../components/assets/massage.jpg'
-import Relaxation from '../../components/assets/relaxation_rock.jpg'
+import Acupuncture from '../../components/assets/acupuncture.jpg'
+import cupping from '../../components/assets/cupping.jpg'
 import Relaxation2 from '../../components/assets/relaxation_rock2.jpg'
+import environment from '../../components/assets/mesa_massage.jpeg'
 
 export const ImageSlider = [
-  {
-    img: Acupuncture,
-    massage: 'Rejuvenate Your Mind, body, and soul.',
-  },
   {
     img: massage,
     massage: 'Rejuvenate Your Mind, body, and soul2.',
   },
   {
-    img: Relaxation,
+    img: Acupuncture,
+    massage: 'Rejuvenate Your Mind, body, and soul.',
+  },
+  {
+    img: cupping,
     massage: 'Rejuvenate Your Mind, body, and soul3.',
   },
   {
     img: Relaxation2,
+    massage: 'Rejuvenate Your Mind, body, and soul4.',
+  },
+  {
+    img: environment,
     massage: 'Rejuvenate Your Mind, body, and soul4.',
   },
 ]
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,11 +2,13 @@ import style from './style.module.scss'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { ImageSlider } from './image_slider'
 import { EffectFade } from 'swiper/modules';
+import Avatar from '../../components/assets/avatar.png'
 
 export const Home = () => {
   return (
     <div className={style.container}>
       <Swiper
+        className={style.swiper_container}
         slidesPerView={ 1 }
         pagination={{clickable: true}}
         navigation
@@ -16,7 +18,7 @@ export const Home = () => {
       >
         {
           ImageSlider.map((item, index) => (
-            <SwiperSlide key={ index } className={style.swiper_container} >
+            <SwiperSlide key={ index } className={style.swiperslide} >
               <img className={style.slide_img} src={ item.img } alt="" />
               <p className={style.slide_message}>
                 {item.massage}
@@ -26,6 +28,9 @@ export const Home = () => {
         }
       </Swiper>
 
+      <div className={style.avatar}>
+        <img src={Avatar} alt="" />
+      </div>
 
     </div>
   )
